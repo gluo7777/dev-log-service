3a9f338128484dcf76dc8b7f00d78d26379e1e4a
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-30 17:27:24 -0600

    modified client

--- src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
+++ src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
@@ -3,13 +3,10 @@ package com.loggerproject.logservice.client.service;
 import com.loggerproject.logservice.server.controller.api.model.get.RequestGetLogModel;
 import com.loggerproject.logservice.server.data.model.LogModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -19,8 +16,7 @@ public class LogServiceClient {
     RestTemplate restTemplate;
 
     public List<LogModel> get(RequestGetLogModel request) {
-        ParameterizedTypeReference<List<LogModel>> typeRef = new ParameterizedTypeReference<List<LogModel>>() {};
-        ResponseEntity<List<LogModel>> responseEntity = restTemplate.exchange("http://LOG-SERVICE/api/get", HttpMethod.POST, new HttpEntity<>(request), typeRef);
-        return responseEntity.getBody();
+        LogModel[] logModels = restTemplate.postForObject("http://LOG-SERVICE/api/get", request, LogModel[].class);
+        return Arrays.asList(logModels);
     }
 }
