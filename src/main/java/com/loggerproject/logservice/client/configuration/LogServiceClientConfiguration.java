ea7db4a24daf882095457091a10bd97d21372c9c
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-30 17:00:05 -0600

    fixed client package scan

--- src/main/java/com/loggerproject/logservice/client/configuration/LogServiceClientConfiguration.java
+++ src/main/java/com/loggerproject/logservice/client/configuration/LogServiceClientConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ComponentScan(basePackages = {"com.loggerproject.globalresource.microservice.log.annotation.client.component"})
+@ComponentScan(basePackages = {"com.loggerproject.logservice.client.service"})
 @RibbonClient(name="log-service", configuration = LogServiceClientRibbonConfiguration.class)
 public class LogServiceClientConfiguration {
 
--- src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
+++ src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
@@ -1,9 +1,16 @@
 package com.loggerproject.logservice.client.service;
 
+import com.loggerproject.logservice.server.controller.api.model.get.RequestGetLogModel;
 import com.loggerproject.logservice.server.data.model.LogModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class LogServiceClient {
@@ -11,7 +18,9 @@ public class LogServiceClient {
     @Autowired
     RestTemplate restTemplate;
 
-    public LogModel getLogModel(String logID) {
-        return this.restTemplate.getForObject("http://LOG-SERVICE/api/get/" + logID, LogModel.class);
+    public List<LogModel> get(RequestGetLogModel request) {
+        ParameterizedTypeReference<List<LogModel>> typeRef = new ParameterizedTypeReference<List<LogModel>>() {};
+        ResponseEntity<List<LogModel>> responseEntity = restTemplate.exchange("http://LOG-SERVICE/api/get", HttpMethod.POST, new HttpEntity<>(request), typeRef);
+        return responseEntity.getBody();
     }
 }
