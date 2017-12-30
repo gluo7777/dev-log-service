a56bcc0b4c67df69668ee13b43fea6d27143cf11
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-30 17:34:22 -0600

    fixed client restTemplate

--- src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
+++ src/main/java/com/loggerproject/logservice/client/service/LogServiceClient.java
@@ -1,13 +1,10 @@
 package com.loggerproject.logservice.client.service;
 
 import com.loggerproject.logservice.server.controller.api.model.get.RequestGetLogModel;
-import com.loggerproject.logservice.server.data.model.LogModel;
+import com.loggerproject.microserviceglobalresource.pojo.controller.response.ResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class LogServiceClient {
@@ -15,8 +12,7 @@ public class LogServiceClient {
     @Autowired
     RestTemplate restTemplate;
 
-    public List<LogModel> get(RequestGetLogModel request) {
-        LogModel[] logModels = restTemplate.postForObject("http://LOG-SERVICE/api/get", request, LogModel[].class);
-        return Arrays.asList(logModels);
+    public ResponseModel get(RequestGetLogModel request) {
+        return restTemplate.postForObject("http://LOG-SERVICE/api/get", request, ResponseModel.class);
     }
 }
