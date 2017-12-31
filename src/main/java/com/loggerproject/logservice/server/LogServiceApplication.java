bebbc0b605058cfd18b6ffd2dc1d53b9b5c10842
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-31 17:41:25 -0600

    global-resourced MongoDBConfig

--- src/main/java/com/loggerproject/logservice/server/LogServiceApplication.java
+++ src/main/java/com/loggerproject/logservice/server/LogServiceApplication.java
@@ -1,19 +1,13 @@
 package com.loggerproject.logservice.server;
 
+import com.loggerproject.microserviceglobalresource.annotation.mongo.MongoDBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @SpringBootApplication -
- * @EnableEurekaClient -
- * @ResfreshScope - allows this config client to refresh the @Value application configurations
- * without it, POST call to localhost:port/refresh will not update the @Value variable
- */
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
+@MongoDBConfig
 public class LogServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/loggerproject/logservice/server/config/CustomConfig.java
@@ -0,0 +1,11 @@
+package com.loggerproject.logservice.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Custom Config specific to this service application
+ */
+@Configuration
+public class CustomConfig {
+
+}
